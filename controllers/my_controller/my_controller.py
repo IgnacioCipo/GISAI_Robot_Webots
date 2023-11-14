e5464b2d0bbc30ede18273dce2efb4199c62a009
IgnacioCipo <ignaciojcipolatti@gmail.com>
2023-11-14 00:45:28 -0300

    Controller charged

---
+++ controllers/my_controller/my_controller.py
@@ -0,0 +1,23 @@
+from controller import Robot
+
+
+robot = Robot()
+timestep = 64
+max_speed = 20
+
+left_motor = robot.getMotor('motor_1')
+right_motor = robot.getMotor('motor_2')
+
+left_motor.setPosition(float('inf'))
+left_motor.setVelocity(0.0))
+
+right_motor.setPosition(float('inf'))
+right_motor.setVelocity(0.0))
+
+
+while robot.step(timestep) != -1:
+    left_speed = 0.5 * max_speed
+    right_speed = 0.5 * max_speed
+    
+    left_motor.setVelocity(left_speed)
+    right_motor.setVelocity(right_speed)
