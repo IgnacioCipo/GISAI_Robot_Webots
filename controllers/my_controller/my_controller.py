47f86a8d510aa22ca7471eb43e329c7efdd9b5dc
IgnacioCipo <ignaciojcipolatti@gmail.com>
2023-11-22 11:00:46 -0300

    Obstacles added

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -3,11 +3,13 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+from controller import DistanceSensor
 
 if __name__ == "__main__":
 
     # create the Robot instance.
     robot = Robot()
+    front_middle_ds = DistanceSensor('ds_front_middle')
     
     # get the time step of the current world.
     timestep = 64
@@ -26,11 +28,15 @@ if __name__ == "__main__":
     right_motor.setPosition(float('inf'))
     right_motor.setVelocity(0.0)
     
+    front_middle_ds.enable(10)
+    
     # Main loop:
     # - perform simulation steps until Webots is stopping the controller
     while robot.step(timestep) != -1:
-        left_speed = 0.7 * max_speed
-        right_speed = 0.2 * max_speed    
+        left_speed = -0.7 * max_speed
+        right_speed = -0.7 * max_speed    
         
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
+        
+        print(front_middle_ds.getValue())
